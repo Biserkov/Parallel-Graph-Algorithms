e947e46ab2fd2802a05630264b886b4b282da7fc
Jordan Biserkov <biserkov@gmail.com>
2011-02-27 16:58:37 +0200

    removed magical constants

--- readability.h
+++ readability.h
@@ -3,12 +3,13 @@
 
 MPI_Comm world = MPI_COMM_WORLD;
 int rootID = 0;
+const int message_size = 1;
 
 MPI_Datatype dtype = MPI_INT;
 
 inline int bcast(void* number, int root, MPI_Comm comm = world)
 {
-	return MPI_Bcast(number, 1, dtype, root, comm);
+	return MPI_Bcast(number, message_size, dtype, root, comm);
 }
 
 inline int myID(MPI_Comm comm = world)
@@ -20,22 +21,22 @@ inline int myID(MPI_Comm comm = world)
 
 inline int scatter(void* sendbuf, void *recvbuf, int root, MPI_Comm comm = world)
 {
-	return MPI_Scatter(sendbuf, 1, dtype, recvbuf, 1, MPI_INT, root, comm);
+	return MPI_Scatter(sendbuf, message_size, dtype, recvbuf, message_size, MPI_INT, root, comm);
 }
 
 inline int gather(void* sendbuf, void *recvbuf, int root, MPI_Comm comm = world)
 {
-  return MPI_Gather( sendbuf, 1, dtype, recvbuf, 1, dtype, root, comm);
+  return MPI_Gather( sendbuf, message_size, dtype, recvbuf, message_size, dtype, root, comm);
 }
 
 inline void send(void* number, int destination, int tag, MPI_Comm comm = world)
 {
-	MPI_Send(number, 1, dtype, destination, tag, comm);
+	MPI_Send(number, message_size, dtype, destination, tag, comm);
 }
 
 inline int recv(               int source,      int tag, MPI_Comm comm = world)
 {
 	MPI_Status status; int number;
-	MPI_Recv(&number, 1, dtype, source, tag, comm, &status);
+	MPI_Recv(&number, message_size, dtype, source, tag, comm, &status);
 	return number;
 }
